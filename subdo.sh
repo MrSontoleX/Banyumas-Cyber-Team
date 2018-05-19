5f245af1abb3ade516426b0e13f2753123547d21
MrSontoleX <37412133+MrSontoleX@users.noreply.github.com>
2018-05-20 00:29:27 +0700

    Add files via upload

---
+++ subdo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear
+echo " =================== Welcome To My Tools ===================";
+echo 'The  _____     _            _____           _     ';
+echo '    |  ___|_ _| | _____    |_   _|__   ___ | |___ ';
+echo '    | |_ / _` | |/ / _ \_____| |/ _ \ / _ \| / __|';
+echo '    |  _| (_| |   <  __/_____| | (_) | (_) | \__ \';
+echo '    |_|  \__,_|_|\_\___|     |_|\___/ \___/|_|___/ v.1.0';
+echo ' ---------------- Coded By Mrsontolex/Muhammad Zainul Ramadhan ---------------';
+echo '';
+read -p "Domain : http://" domain
+read -p "Delay? (1-5 ) :" delay
+read -p "Jumlah? (1-100000):" jumlah
+read -p "Output (SCTkntl.txt) :" output
+echo "[+] Generating..."
+count=1
+while [ $count -le $jumlah ]
+do
+printf "http://$RANDOM.$domain\n" >> $output
+echo "[$count] Generated => data/$output"
+sleep $delay
+(( count++ ))
+done
+echo "[+] Success"
